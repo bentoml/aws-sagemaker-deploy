7d8c1fc8f8a540723f2e540eee08f2bee68129e5
jjmachan <jamesjithin97@gmail.com>
2022-04-06 00:11:27 +0530

    new functions for operator to generate, create_deployable and registry_info

--- bentoctl_sagemaker/__init__.py
+++ bentoctl_sagemaker/__init__.py
@@ -1,4 +1,5 @@
+from bentoctl_sagemaker.create_deployable import create_deployable
+from bentoctl_sagemaker.generate import generate
+from bentoctl_sagemaker.registry_utils import get_registry_info
 
-
-__all__ = [
-]
+__all__ = ["generate", "create_deployable", "get_registry_info"]
