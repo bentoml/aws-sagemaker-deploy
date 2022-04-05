ec65fb9992b2d9761f2f6a5a9807e70ae1c24570
jjmachan <jamesjithin97@gmail.com>
2022-04-05 18:23:41 +0530

    restructure the operator

--- bentoctl_sagemaker/__init__.py
+++ bentoctl_sagemaker/__init__.py
@@ -1,15 +1,4 @@
-from .deploy import deploy
-from .update import update
-from .describe import describe
-from .delete import delete
-from .utils import console, get_configuration_value
 
 
 __all__ = [
-    'deploy',
-    'update',
-    'describe',
-    'delete',
-    'console',
-    'get_configuration_value',
 ]
