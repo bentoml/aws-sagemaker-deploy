2c0aead55ecc4cff64ab6c5d122777ada46cc23c
jjmachan <jamesjithin97@gmail.com>
2022-07-29 17:48:42 +0530

    feat: better help messages and default when directly invoking sagemaker

--- bentoctl_sagemaker/sagemaker/service.py
+++ bentoctl_sagemaker/sagemaker/service.py
@@ -1,8 +1,8 @@
+import logging
+
 import bentoml
-from starlette.applications import Starlette
+from bentoml.exceptions import BentoMLException
 from starlette.requests import Request
-from starlette.responses import JSONResponse
-from starlette.routing import Mount, Route
 from starlette.types import ASGIApp, Receive, Scope, Send
 
 AWS_SAGEMAKER_SERVE_PORT = 8080
@@ -28,8 +28,29 @@ class SagemakerMiddleware:
                 scope["path"] = BENTOML_HEALTH_CHECK_PATH
 
             if req.url.path == "/invocations":
-                assert AWS_CUSTOM_ENDPOINT_HEADER in req.headers
-                api_path = req.headers[AWS_CUSTOM_ENDPOINT_HEADER]
+                if AWS_CUSTOM_ENDPOINT_HEADER not in req.headers:
+                    if len(svc.apis) == 1:
+                        # only one api, use it
+                        api_path, *_ = svc.apis
+                        logging.warning(
+                            f"'{AWS_CUSTOM_ENDPOINT_HEADER}' not found in request header. Using defualt {api_path} service."
+                        )
+                    else:
+                        logging.error(
+                            f"'{AWS_CUSTOM_ENDPOINT_HEADER}' not found inside request header. If you are directly invoking the Sagemaker Endpoint pass in the '{AWS_CUSTOM_ENDPOINT_HEADER}' with the bentoml service name that you want to invoke."
+                        )
+                        raise BentoMLException(
+                            f"'{AWS_CUSTOM_ENDPOINT_HEADER}' not found inside request header."
+                        )
+                else:
+                    api_path = req.headers[AWS_CUSTOM_ENDPOINT_HEADER]
+                    if api_path not in svc.apis:
+                        logging.error(
+                            "API Service passed via the '{AWS_CUSTOM_ENDPOINT_HEADER}' not found in the bentoml service."
+                        )
+                        raise BentoMLException(
+                            "API Service passed via the '{AWS_CUSTOM_ENDPOINT_HEADER}' not found in the bentoml service."
+                        )
                 scope["path"] = "/" + api_path
 
         await self.app(scope, receive, send)
